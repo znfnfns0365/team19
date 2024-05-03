87605db64032882a5377092b8846a18657cbb317
kdh <znfnfns0365@naver.com>
2024-05-03 16:11:30 +0900

    to pull

--- reviewEdit.js
+++ reviewEdit.js
@@ -2,6 +2,8 @@ const modelView = document.querySelector(".modal");
 const modalOpen = document.querySelector("#editButton");
 const modal = document.querySelector(".modal");
 const cancelButton = document.querySelector("#cancelButton");
+const buttonEditDelete = document.querySelector(".btn.btn-danger");
+const makeButton = document.querySelector("#makeButton");
 
 
 modalOpen.addEventListener('click',function(){
@@ -11,3 +13,12 @@ modalOpen.addEventListener('click',function(){
 cancelButton.addEventListener('click',function(){
     modal.style.display = 'none';
 })
+
+buttonEditDelete.addEventListener('click',function(){
+    
+})
+
+makeButton.addEventListener('click', function(){
+    
+    localStorage.setItem('key', 'value');
+});
