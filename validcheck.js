2acf532cb9ab212a6626ea525b32818293d19d4a
SW-64 <ettjddns6@gmail.com>
2024-05-03 21:18:24 +0900

    2차

--- validcheck.js
+++ validcheck.js
@@ -1,8 +1,10 @@
-const review_msg = document.getElementById("msg");
-const review_name = document.getElementById("exampleFormControlInput1");
-const review_pwd = document.getElementById("exampleFormControlInput2");
-const review_error = document.getElementById("review_error");
-const review_btn = document.getElementById("review_btn");
+const review_error = document.querySelector(".review_error");
+const review_msg = document.querySelector(".review_error");
+const review_name = document.querySelector(".review_name");
+const review_pwd = document.querySelector(".review_pwd");
+const review_btn = document.querySelector(".review_btn");
+
+
 // 아이디: 글자 수 제한 ( 2글자~12글자)
 function idLength(value) {
     return value.length >= 2 && value.length <= 12
@@ -20,17 +22,17 @@ function strongPassword(str) {
 }
 // 아이디 이벤트 
 review_name.onkeyup = function () {
-    // 에러값 초기화
     let return_value = false;
+    //에러값 초기화
     review_error.innerHTML = '';
     if (review_name.value.length !== 0) {
         // 영어 또는 숫자 외의 값을 입력했을 경우
         if (onlyNumberAndEnglish(review_name.value) === false) {
-            review_error.innerHTML += '<p>아이디 오류 : 영어 혹은 숫자만 입력하시오</p>'
+            review_error.innerHTML = ("<p>아이디 오류 : 영어 혹은 숫자만 입력하시오</p>");
         }
         // 길이 미만 혹은 초과일 경우
         else if (idLength(review_name.value) === false) {
-            review_error.innerHTML += '<p>아이디 오류 : 2~12글자이어야 합니다. </p>'
+            review_error.innerHTML = "<p>아이디 오류 : 2~12글자이어야 합니다. </p>";
         }
         else {
             return_value = true;
@@ -40,13 +42,13 @@ review_name.onkeyup = function () {
 }
 // 비밀번호 이벤트
 review_pwd.onkeyup = function () {
-    // 에러값 초기화
     let return_value = false;
+    //에러값 초기화
     review_error.innerHTML = '';
     // 8글자 이상, 영문, 숫자, 특수문자 외의 값을 입력했을경우
     if (review_pwd.value.length !== 0) {
         if (strongPassword(review_pwd.value) === false) {
-            review_error.innerHTML += '<p>비밀번호 오류 : 8글자 이상, 영문, 숫자, 특수문자이어야 합니다.</p>';
+            review_error.innerHTML = '<p>비밀번호 오류 : 8글자 이상, 영문, 숫자, 특수문자이어야 합니다.</p>';
         }
         else {
             return_value = true;
