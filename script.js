619546c91e61acf041192652c6bfa243f157eddb
znfnfns0365 <znfnfns0365@naver.com>
2024-05-07 18:15:29 +0900

    sort complete

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const searchInput = document.getElementById("searchInput");
 const editId = document.getElementById("cardSpace");
 const cardsClick = document.querySelector(".cards");
 const titleClick = document.getElementById("title");
+const sortTitle = document.getElementById("sortTitle");
+const sortRating = document.getElementById("sortRating");
+const sortRelease = document.getElementById("sortRelease");
 
 const options = {
   method: "GET",
@@ -16,7 +19,7 @@ const options = {
 const fetchMovie = async () => {
   const data = await fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", options);
   const dataResponsed = await data.json();
-  displayMovies(dataResponsed);
+  displayMovies(dataResponsed, "sortTitle");
   makeEvent(dataResponsed);
 };
 
@@ -31,8 +34,6 @@ const search = (data) => {
     return;
   }
   editId.innerHTML = null;
-  const element = document.querySelector(".cards");
-  // element.style.margin = "0px";
   for (let i = 0; i < filteredArr.length; i++) {
     editId.innerHTML += getMovieCode(filteredArr[i]);
   }
@@ -65,6 +66,16 @@ const makeEvent = (data) => {
   titleClick.addEventListener("click", () => {
     location.reload(true);
   });
+
+  sortTitle.addEventListener("click", function () {
+    displayMovies(data, "sortTitle");
+  });
+  sortRating.addEventListener("click", function () {
+    displayMovies(data, "sortRating");
+  });
+  sortRelease.addEventListener("click", function () {
+    displayMovies(data, "sortRelease");
+  });
 };
 
 function getMovieCode(movie) {
@@ -77,30 +88,31 @@ function getMovieCode(movie) {
   return movieCode;
 }
 
-function displayMovies(data) {
+function displayMovies(data, howToSort) {
+  editId.innerHTML = null;
   let sortedData = (sortStd) => {
-    if (sortStd === "제목순") {
+    if (sortStd === "sortTitle") {
       return data.results.sort((a, b) => {
         if (a.title > b.title) return 1;
         else if (a.title < b.title) return -1;
         return 0;
       });
-    } else if (sortStd === "평점순") {
+    } else if (sortStd === "sortRating") {
       return data.results.sort((a, b) => {
-        if (a.vote_average > b.vote_average) return 1;
-        else if (a.vote_average < b.vote_average) return -1;
+        if (a.vote_average < b.vote_average) return 1;
+        else if (a.vote_average > b.vote_average) return -1;
         return 0;
       });
     } else {
       // 개봉일순
       return data.results.sort((a, b) => {
-        if (a.release_date > b.release_date) return 1;
-        else if (a.release_date < b.release_date) return -1;
+        if (a.release_date < b.release_date) return 1;
+        else if (a.release_date > b.release_date) return -1;
         return 0;
       });
     }
   };
-  data = sortedData("개봉일순");
+  data = sortedData(howToSort);
   for (let movie of data) {
     editId.innerHTML += getMovieCode(movie);
   }
